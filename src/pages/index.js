25962f1d8749b797dba309cf34ff9f2586ea77fc
Fin Donaghey <fw.donaghey@outlook.com.au>
2022-07-01 01:49:30 +1000

    Finished Gatsby Tutorial Part 2

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Crypto Geckos">
+    <p>Living in the branches of the Solana Blockchain.</p>
+  </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default IndexPage
